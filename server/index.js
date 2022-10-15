a8c6ce440b87dfd2dfd3d683214659f05508d64c
Abir Haque <abir.haque.usa@gmail.com>
2022-10-14 23:39:28 -0500

    Added get_project route
    Finally able to communicate with MongoDB with NodeJS.

--- server/index.js
+++ server/index.js
@@ -8,25 +8,30 @@ const mongodb_user = process.env.MONGODB_USER
 const mongodb_pass = process.env.MONGODB_PASS
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://"+mongodb_user+":<"+mongodb_pass+">@"+mongodb_user+".rqyruvg.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const ObjectId = require("mongodb").ObjectId;
+const uri = "mongodb+srv://"+mongodb_user+":"+mongodb_pass+"@hackwashu2022.rqyruvg.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useUnifiedTopology: true});
 
 var jsonParser = bodyParser.json()
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-	res.send('Bruh lmao');
+	res.send('Hack WashU 2022 API');
+});
+
+app.get('/get_project/:id', (req, res) => {
+    
+  var query = {_id : ObjectId(req.params.id)}
+  const collection = client.db("hackwashu2022").collection("projects");
+  collection.find(query).toArray(function(err,result){
+    res.send(result)
+	});
 });
 
 
-
-
-
-
-
-
-client.connect(err => {
-  //const collection = client.db("test").collection("devices");
-  //client.close();
-  console.log("AYYY LMAO\n MONGODB CONNECTED!!!")
+app.listen(process.env.PORT||3000, () => {
+	client.connect(err => {
+        console.log("AYYY LMAO\n MONGODB CONNECTED!!!")
+	})
+  console.log('Server started.');
 });
